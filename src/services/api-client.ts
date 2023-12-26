1de6b633948a6b807fad53140aed7951c178ca28
moinuddinrao <moinrajput594@gmail.com>
2023-12-26 23:46:58 +0500

    fix: configure environment variables

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "2e74f08e445c4f69862a83b8e850e640",
+    key: import.meta.env.VITE_RAWG_API_KEY,
   },
 });
